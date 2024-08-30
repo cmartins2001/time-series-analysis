d816d5a9e367c86e6ab86d8b71c4d5231ae423b1
cmartins2001 <cmartinspfhs2001@gmail.com>
2024-08-30 09:07:00 -0400

    added another data file for testing misnamed date columns.

--- shake_rust/structure.py
+++ shake_rust/structure.py
@@ -15,18 +15,41 @@ class TimeSeriesDataLoader:
     def __init__(self, name):
         self.path = os.path.join(repo_dir, name)
         self.data = pd.read_csv(self.path)
-        self.date_col = self.data['date']
+
+        if 'date' in self.data.columns:
+            self.date_col = self.data['date']
+        else:
+            self.date_col = None
 
     # def load_csv(self):
     #     return pd.read_csv(self.path)
     
     def set_date_col(self):
         if self.date_col.dtype != None:
-            if self.date_col.dtype == "datetime64":
-                print("Date column is already in datetime format.")
+
+            # Check if the date column is already a datetime dtype:
+            if self.date_col.dtype == "datetime64[ns]":
+
+                # Change nothing:
+                self.date_col = self.date_col
+
             else:
+
+                # Convert existing column to pandas datetime format:
                 self.date_col = pd.to_datetime(self.date_col)
-                print("Date column was SET to datetime format.")
         else:
-            print("There is no column named 'date', please check source data and try again.")
+
+            # Cycle through columns and look for datetime columns:
+            for col in self.data.columns:
+
+                # Extract date range if a datetime column is detected:
+                if self.data[col].dtype == "datetime64[ns]":
+
+                    # Define the date range"
+                    max_date = self.data[col].max()
+                    min_date = self.data[col].min()
+                    date_range = f'{min_date} to {max_date}'
+
+                    # Output col name and range:
+                    print(f"{self.data[col].name}: {date_range}")
 
